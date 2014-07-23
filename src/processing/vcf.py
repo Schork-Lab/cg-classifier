3935d4388e8c6346fc6fdb1469a5638a4568180b
Kunal Bhutani <kunalbhutani@gmail.com>
2014-07-22 23:56:07 -0700

    Initial foray into the representation of a vcf file. Issues #5 and #6 . Definitely can change structure of the package i.e. processing and the module i.e. vcf, but I think the overall idea for how the vcf should be loaded and features extracted is in there.

---
+++ src/processing/vcf.py
@@ -0,0 +1,103 @@
+'''
+Created on Jul 22, 2014
+
+@author: Kunal Bhutani
+'''
+
+class VCF(object):
+    '''
+    pdVCF class. 
+    
+    Note: It's frowned upon / hacky to inherit dataframe class, so we will have to work with a 
+    VCF class and its representation.
+    '''
+    features = ['DP']
+
+    def __init__(self, filename, filetype='vcf'):
+        self._df = load_vcf(filename)
+        self._process()
+        return
+
+    @property
+    def df(self):
+        return self._df
+
+    @property
+    def features(self):
+        return self._df[features].values
+
+    def _process(self):
+
+        self._df['variant_type'] = self._df.apply(_variant_type, axis=1)
+        # Add other processing functions
+        return
+
+    def _generate_features(self):
+        # Generate features
+        self._df['DP'] = 0
+        return
+
+    def load_vcf(self, filename):
+        '''
+        Loads in a vcf file, aware of gzipped files.
+        '''
+        compression = None
+        if filename.endswith('.gz'):
+            IN = gzip.open(filename)
+            compression = 'gzip'
+        else:
+            IN = open(filename)
+
+        num_of_header = 0
+        for line in IN:
+            if line.startswith('#'):
+                num_of_header += 1
+            else:
+                break
+        IN.close()
+
+        df = pd.read_table(filename, sep='\t', compression=compression,
+                            skiprows=num_of_header - 1)
+        df.set_index(['#CHROM', 'POS', 'REF', 'ALT'], drop=False, inplace=True)
+        return df
+
+    def _variant_type(series):
+        def is_snp(ref, alt):
+            """ Return whether or not the variant is a SNP """
+            if len(ref) != len(alt): return False
+            base_diff = [nt for i, nt in enumerate(alt) if ref[i] != alt[i]]
+            if len(base_diff) <= 1:
+                    return True
+            return False
+
+
+        def is_mnp(ref, alt):
+            """ Return whether or not the variant is an MNP"""
+            if len(ref) > 1 and len(ref) == len(alt):
+                return True
+
+        def is_insertion(ref, alt):
+            if len(ref) < len(alt):
+                return True
+            return False
+
+        def is_del(ref, alt):
+            if len(ref) > len(alt):
+                return True
+            return False
+
+
+        ref = series['REF']
+        alt = series['ALT']
+        if is_snp(ref, alt):
+            return 'snp'
+        elif is_mnp(ref, alt):
+             return 'mnp'
+        elif is_insertion(ref, alt):
+            return 'ins'
+        elif is_del(ref, alt):
+            return 'del'
+        else:
+            return 'indel or SV'
+
+

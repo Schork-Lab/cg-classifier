87e512ff7090db09cf95ced0dcd70d0fa27b4f16
kunal <kunalbhutani@gmail.com>
2014-07-23 12:07:03 -0700

    Updated with the logic of the preprocess_vcf_line function. There are definitely many avenues for speeding up the process as the same series is being passed in many different ways. Also new pandas allows for the creation of columns inside the apply function, which can be used to speed this up immensely. Issue #6, #7

--- cg-classifier/processing/variants.py
+++ cg-classifier/processing/variants.py
@@ -6,7 +6,17 @@ Created on Jul 22, 2014
 import pandas as pd
 
 class VariantFile(object):
-    features = ['DP']
+
+    index = ['CHROM', 'POS', 'REF', 'ALT']
+    features = {'allele': ['HQ', 'EHQ', 'CGA_CEHQ', 'AD'],
+                'binary': ['CGA_XR', 'CGA_RPT', 'multiallele'],
+                'categorical': ['FT', 'vartype1', 'vartype2', 'phase', 'zygosity'],
+                'numeric': ['FT', 'vartype1', 'vartype2', 'phase', 'zygosity']
+                }
+
+    all_features = [feature
+                    for subfeatures in features.itervalues()
+                    for feature in subfeatures]
 
     def __init__(self):
         self._df = None
@@ -17,9 +27,102 @@ class VariantFile(object):
 
     @property
     def features(self):
+
         return self._df[features].values
 
-    def _variant_type(series):
+    @staticmethod
+    def _get_phase(series, gt_column):
+        '''
+        
+        :param series:
+        :type series:
+        :param gt_column:
+        :type gt_column:
+        '''
+        '''
+        Returns phase status from a GT field
+        '''
+        if "|" in series[gt_column]:
+            return "|"
+        if "/" in series[gt_column]:
+            return "/"
+        else:
+            return '-'
+
+    @staticmethod
+    def _get_allele(series, sample_column, phase_column,
+                    chromosome_num=0):
+        '''
+        Using the field info field in a sample column and the phasing
+        field, parses out the allele from the first or second chromosome.
+        
+        :param series:
+        :type series:
+        :param sample_column:
+        :type sample_column:
+        :param phase_column:
+        :type phase_column:
+        :param chromosome_num:
+        :type chromosome_num:
+        '''
+
+        # Assumes first info field is GT
+        gt_split = series[phase_column]
+        allele_calls = series[sample_column].split(":")[0].split(gt_split)
+        alleles = [series['REF']] + series['ALT'].split(",")
+        try:
+            allele_number = int(allele_calls[chromosome_num])
+            allele = alleles[allele_number]
+        except:
+            allele = "."
+        return allele
+
+    @staticmethod
+    def _get_zygosity(series):
+        '''
+        Based on the alleles present in sample, determines the zygosity
+        of the individual in comparison to the reference.
+        
+        :param series:
+        :type series:
+        '''
+
+        allele_set = set([series['a1'], series['a2'], series['REF']])
+        num_of_alleles = len(allele_set)
+
+        if num_of_alleles == 1:
+            return 'hom-ref'
+
+        elif (num_of_alleles == 2  and
+            series['a1'] == series['a2']):
+            return 'hom-alt'
+
+        elif (num_of_alleles == 2 and
+              series['a1'] != series['a2']):
+            return 'het-ref'
+
+        elif num_of_alleles == 3 :
+            return 'het-alt'
+
+        else:
+            assert False
+
+    @staticmethod
+    def _get_vartype(series, allele_base_col):
+        '''
+        This function assigns the following vartypes to the 
+        allele specified by allele_base_col: snp, mnp, ins, del, indel or SV
+        
+        '''
+        ref = series['REF']
+        alt = series[allele_base_col]
+        if is_snp(ref, alt): return 'snp'
+        elif is_sv(ref, alt): return 'sv'
+        elif is_mnp(ref, alt): return 'mnp'
+        elif is_insertion(ref, alt): return 'ins'
+        elif is_del(ref, alt): return 'del'
+        else: return 'indel or SV'
+
         def is_snp(ref, alt):
             """ Return whether or not the variant is a SNP """
             if len(ref) != len(alt): return False
@@ -28,11 +131,16 @@ class VariantFile(object):
                     return True
             return False
 
+        def is_sv(ref, alt):
+            if "[" in alt or "]" in alt:
+                return True
+            return False
 
         def is_mnp(ref, alt):
             """ Return whether or not the variant is an MNP"""
             if len(ref) > 1 and len(ref) == len(alt):
                 return True
+            return False
 
         def is_insertion(ref, alt):
             if len(ref) < len(alt):
@@ -45,23 +153,9 @@ class VariantFile(object):
             return False
 
 
-        ref = series['REF']
-        alt = series['ALT']
-
-        if is_snp(ref, alt):
-            return 'snp'
-        elif is_mnp(ref, alt):
-            return 'mnp'
-        elif is_insertion(ref, alt):
-            return 'ins'
-        elif is_del(ref, alt):
-            return 'del'
-        else:
-            return 'indel or SV'
-
-class VCF(VariantFile):
+class VcfTsv(VariantFile):
     def __init__(self, filename):
-        self._df = self._load_vcf(filename)
+        self._df = self._load_vcf_tsv(filename)
         self._process()
         return
 
@@ -72,10 +166,43 @@ class VCF(VariantFile):
         
         '''
 
-        # Overall processing functions
-        self._df['variant_type'] = self._df.apply(_variant_type, axis=1)
+        df = self._df
 
-        # Add other processing functions
+        # Overall processing functions
+        self._df['variant_type'] = self._df.apply(self._variant_type, axis=1)
+
+        # Ignore CNV and Repeats
+        discard_vars = ['<INS:ME:ALU>', '<INS:ME:L1>', '<CGA_CNVWIN>', '<INS:ME:SVA>']
+        df = df[~df.ALT.isin(discard_vars)]
+
+        # Phase genotype and remove edge cases, likely at sex chromosome sites
+        df['phase'] = df.apply(self._get_phase, args=['GT'], axis=1)
+        df = df[df.phase != "-"]
+
+        # Get allele base sequences
+        # TODO: Combine phasing and getting alleles into one step
+        # i.e. df['a1'], df['a2'] = df['GT'].map(lambda x: x.replace('|','/').split('/'))
+        df['a1'] = df.apply(self._get_allele, args=['GT', 'phase', 0], axis=1)
+        df['a2'] = df.apply(self._get_allele, args=['GT', 'phase', 1], axis=1)
+
+        # Multiallelic site if more than one alternative allele
+        df['multiallele'] = df['ALT'].map(lambda x: 1 if "," in x else 0)
+
+        # Zygosity: hom-ref, het-ref, hom-alt, het-alt
+        df['zygosity'] = df.apply(self._get_zygosity, axis=1)
+
+        # Set the variant type based on number of bases in reference versus allele
+        df['vartype1'] = df.apply(self._get_vartype, args=['a1'], axis=1)
+        df['vartype2'] = df.apply(self._get_vartype, args=['a2'], axis=1)
+
+        # Ignore structural variants
+        df = df[(df.vartype1 != 'sv') & (df.vartype2 != 'sv')]
+
+        # Fill missing allelic depth with 0,0 reads
+        df['AD'].fillna(value='0,0', inplace=True)
+
+        # Get number of reads supporting the reference allele
+        df['ref_read_depth'] = df.apply(ref_read_depth, axis=1)
 
         # Generate features
         self._generate_features()
@@ -93,26 +220,14 @@ class VCF(VariantFile):
         '''
         compression = None
         if filename.endswith('.gz'):
-            IN = gzip.open(filename)
             compression = 'gzip'
-        else:
-            IN = open(filename)
 
-        num_of_header = 0
-        for line in IN:
-            if line.startswith('#'):
-                num_of_header += 1
-            else:
-                break
-        IN.close()
-
-        df = pd.read_table(filename, sep='\t', compression=compression,
-                            skiprows=num_of_header - 1)
+        df = pd.read_table(filename, sep='\t', compression=compression)
         df.set_index(['#CHROM', 'POS', 'REF', 'ALT'], drop=False, inplace=True)
         return df
 
 
-class masterVar(VariantFile):
+class MasterVar(VariantFile):
 
     def __init__(self, filename):
         self._df = self._load_master(filename)

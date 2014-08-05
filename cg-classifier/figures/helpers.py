99347912c075e229d2334b8c25ff33d779bffa1b
Kunal <kunalbhutani@gmail.com>
2014-08-04 19:01:24 -0700

    Cleaned up figure1 and figure2 code. Added PPV value.

--- cg-classifier/figures/helpers.py
+++ cg-classifier/figures/helpers.py
@@ -5,12 +5,14 @@ Created on Jul 25, 2014
 '''
 
 from collections import OrderedDict
+from math import log10
+
+import pandas as pd
 
 import matplotlib
+from matplotlib import rcParams
 import matplotlib.pyplot as plt
 from matplotlib import pylab
-
-
 import brewer2mpl
 import seaborn as sns
 
@@ -75,3 +77,53 @@ def count_box_plot(count_df, colors, ax=None, fig=None):
     frame.set_facecolor('w')
     ax.set_xlim(0 - width, ax.get_xlim()[1] + width)
     return fig, ax
+
+
+def get_count_df(stats):
+    variants = ['ins', 'del', 'mnp', 'snp']
+    count_df = pd.concat([stat['counts'][variants] for stat in stats], axis=1)
+    count_df = count_df.applymap(log10)
+    count_df.index = ['Insertion', 'Deletion', 'MNP', 'SNP']
+    return count_df
+
+
+def plot_counts(fig, ax, count_df, colors):
+    fig, ax = count_box_plot(count_df, colors, ax, fig)
+    ax.set_ylabel('Number of Variants (log10)')  # , fontdict={'size':10})
+    return fig, ax
+
+def my_rstyle(ax):
+    """Styles an axes to appear like ggplot2
+    Must be called after all plot and axis manipulation operations have been carried out (needs to know final tick spacing)
+    """
+    # set the style of the major and minor grid lines, filled blocks
+
+    ax.grid(True, 'major', color='gray', linestyle='-', linewidth=0.7, alpha=0.5)
+    ax.patch.set_facecolor('white')
+    ax.set_axisbelow(True)
+
+    # restyle the tick lines
+    for line in ax.get_xticklines() + ax.get_yticklines():
+        line.set_markersize(5)
+        line.set_color("gray")
+        line.set_markeredgewidth(1.4)
+
+    # remove the minor tick lines
+    for line in ax.xaxis.get_ticklines(minor=True) + ax.yaxis.get_ticklines(minor=True):
+        line.set_markersize(0)
+
+    # only show bottom left ticks, pointing out of axis
+    rcParams['xtick.direction'] = 'out'
+    rcParams['ytick.direction'] = 'out'
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    # set x, y limit
+
+    if ax.legend_ <> None:
+        lg = ax.legend_
+        lg.get_frame().set_linewidth(0)
+        lg.get_frame().set_alpha(0.5)
